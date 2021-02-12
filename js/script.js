edf05e2a2c41d497f5c435e3d71b4241183ba571
Kevin Chen <D1034181036@gm.lhu.edu.tw>
2021-02-12 12:00:49 +0800

    2021/02/12

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('#slides').superslides({
 		animation: 'fade',
 		play: 5000,
@@ -12,4 +13,44 @@ $(document).ready(function(){
 		startDelay: 1000,
 		showCursor: false
 	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	        	items:4
+	        }
+	    }
+	})
+
+	//get skill section position
+    var skillsTopOffset = $("#skills").offset().top;
+
+    //當螢幕到skills高度 - window大小 + 200 時執行動畫
+    $(window).scroll(function(){
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+    		$('.chart').easyPieChart({
+		    	easing: 'easeInOut',
+		    	barColor: '#fff',
+		    	trackColor: false,
+		    	scaleColor: false,
+		    	lineWidth: 4,
+		    	size: 152,
+		    	onStep: function(from, to, percent){
+		    		$(this.el).find('.percent').text(Math.round(percent));
+		    	}
+		    });
+    	}
+    });
+
 })
