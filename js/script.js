3f52fedf133bc656a8a7b7fef0d69f7067ecc7b9
d1034181036 <d1034181036@gm.lhu.edu.tw>
2023-01-05 14:21:00 +0800

    - 優化聯絡資訊 - 調整Footer

--- js/script.js
+++ js/script.js
@@ -46,8 +46,8 @@ $(document).ready(function(){
 	    }
 	})
 
-    //當螢幕到skills高度 - window大小 + 200 時執行動畫
-    var skillsTopOffset = $("#skills").offset().top; //get skill section position
+    // 當螢幕到 (skills 高度 - window大小 + 200) 時執行動畫
+    var skillsTopOffset = $("#skills").offset().top; // get skill section position
 
     $(window).scroll(function(){
     	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
@@ -85,4 +85,40 @@ $(document).ready(function(){
 	    return false;
     })
 
+    
+    // Usage:
+
+    // Example 1:
+    // <input id="url" type="text" value="http://example.com" readonly>
+    // <button data-clipboard-target="#url" data-bs-toggle="tooltip">Copy</button>
+
+    // Example 2:
+    // <button data-clipboard-text="hello world" data-bs-toggle="tooltip">Copy</button>
+
+    function clipboardWithTooltip(target = '[data-bs-toggle="tooltip"]', tooltipText = 'Copied!'){
+        // Initialize Tooltip
+        const tooltipTriggerList = [].slice.call(document.querySelectorAll(target));
+        
+        const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl, {
+                trigger: 'manual',
+                placement: 'bottom',
+            });
+        });
+
+        // Initialize Clipboard
+        const clipboard = new ClipboardJS(target);
+
+        clipboard.on('success', function(e) {
+            $(e.trigger).tooltip('hide').attr('data-bs-original-title', tooltipText).tooltip('show');
+            setTimeout(function() { $(e.trigger).tooltip('hide'); }, 1000);
+        });
+
+        clipboard.on('error', function(e) {
+            $(e.trigger).tooltip('hide').attr('data-bs-original-title', 'Failed!').tooltip('show');
+            setTimeout(function() { $(e.trigger).tooltip('hide'); }, 1000);
+        });
+    }
+
+    clipboardWithTooltip(target = '[data-bs-toggle="tooltip"]', text = 'Copied!');
 })
